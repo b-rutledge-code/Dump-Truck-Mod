a7f1f59fbd341dc7084387c44e4445cd1ef74390
Brian Rutledge <rut.link@gmail.com>
2025-03-15 21:50:16 -0500

    Remove deploy.sh from git tracking

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Simple script to copy the mod to Zomboid/Mods with forced overwrite
-cp -rf Contents/mods/DumpTruckGravelMod "$HOME/Zomboid/Mods/"
-echo "✅ Mod copied to $HOME/Zomboid/Mods/DumpTruckGravelMod"
