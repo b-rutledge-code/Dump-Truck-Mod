2d196899099505dd16d8c5b9f33c944b5d5e0064
Brian Rutledge <rut.link@gmail.com>
2025-11-27 16:24:05 -0600

    Update workshop description with new features and improved formatting

--- Contents/mods/DumpTruckGravelMod/42.0/media/maps/vehicles/vehicles.lua
+++
@@ -1,8 +0,0 @@
-require "Vehicles/vehicles_distributions"
-
-local function initVehicles()
-    -- This function is called when the map is loaded
-    -- The distribution file we created will be loaded automatically
-end
-
-Events.OnLoadMap.Add(initVehicles) 
